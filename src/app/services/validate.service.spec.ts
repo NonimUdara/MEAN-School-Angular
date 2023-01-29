744d9e31f359927a85a571a4a3741daee286d9dd
Nonim Udara <nonimudara123@gmail.com>
2023-01-29 23:53:16 +0530

    Test case fixes

--- src/app/services/validate.service.spec.ts
+++ src/app/services/validate.service.spec.ts
@@ -62,13 +62,13 @@ describe('ValidateService', () => {
   it('Valid Student Register', () => {
     res = service.validateStudentRegister({name: undefined, index: '12345', stream: 'Maths', phone: '0711234567', email: 'kamal@gmamil.com', nic: '701246627282', password: 'kamal@gmail'});
   
-    expect(res).toBe(true);
+    expect(res).toBe(false);
   });
 
   it('Invalid Student Register', () => {
     res = service.validateStudentRegister({name: 'Kamal', index: '12345', stream: 'Maths', phone: '0711234567', email: 'kamal@gmamil.com', nic: '701246627282', password: 'kamal@gmail'});
   
-    expect(res).toBe(false);
+    expect(res).toBe(true);
   });
 
   it('should be created', () => {
